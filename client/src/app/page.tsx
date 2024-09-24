d72bb7d9bd4dc3a322ff7f6d9dee9f0c85e679ff
Dharambir <dev.dharambir@gmail.com>
2024-09-24 22:44:29 +0000

    Removed bug

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,25 +1,25 @@
 "use client";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import Hero from "@/components/Home/Hero";
 
 export default function Home() {
-  const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    console.log("Clicked");
-    try {
-      const data = await fetch(
-        `${process.env.NEXT_PUBLIC_SERVERHOST}/user/register`,
-        {
-          method: "POST",
-          next: { revalidate: 60 },
-        }
-      );
-      const res = await data.json();
-      console.log(res);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  // const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+  //   e.preventDefault();
+  //   console.log("Clicked");
+  //   try {
+  //     const data = await fetch(
+  //       `${process.env.NEXT_PUBLIC_SERVERHOST}/user/register`,
+  //       {
+  //         method: "POST",
+  //         next: { revalidate: 60 },
+  //       }
+  //     );
+  //     const res = await data.json();
+  //     console.log(res);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
   return (
     <div>
       <Hero />
