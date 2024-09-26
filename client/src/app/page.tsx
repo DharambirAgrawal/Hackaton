42540d24b8abd06088c2ebd4674500e2649f90fc
Dharambir <dev.dharambir@gmail.com>
2024-09-26 23:54:33 +0000

    Checking backend

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,29 +1,31 @@
 "use client";
-// import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import Hero from "@/components/Home/Hero";
 
 export default function Home() {
-  // const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
-  //   e.preventDefault();
-  //   console.log("Clicked");
-  //   try {
-  //     const data = await fetch(
-  //       `${process.env.NEXT_PUBLIC_SERVERHOST}/user/register`,
-  //       {
-  //         method: "POST",
-  //         next: { revalidate: 60 },
-  //       }
-  //     );
-  //     const res = await data.json();
-  //     console.log(res);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+  const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log("Clicked");
+    try {
+      const data = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVERHOST}/user/register`,
+        {
+          method: "POST",
+          next: { revalidate: 60 },
+        }
+      );
+      const res = await data.json();
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div>
       <Hero />
-      {/* <Button onClick={handleClick}>Click me </Button> */}
+      <Button onClick={handleClick} className=" bg-red-400">
+        Click me{" "}
+      </Button>
     </div>
   );
 }
