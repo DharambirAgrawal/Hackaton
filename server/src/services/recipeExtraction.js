91b31474a831f8ca9eb7bad531965f9b5d607b5b
ghislainnkundayezu <ghislainnkundayezu@gmail.com>
2024-09-27 15:31:46 -0500

    minor

--- server/src/services/recipeExtraction.js
+++ server/src/services/recipeExtraction.js
@@ -6,25 +6,27 @@ const APP_KEY = 'c9f07d926e0453653f173b5bced350c9';
 
 // Function to search for recipes based on multiple ingredients
 async function searchRecipes(ingredients) {
-    const ingredientsQuery = ingredients.join(',');
-    console.log(ingredientsQuery)
-    const url = `https://api.edamam.com/search?q=${encodeURIComponent(ingredientsQuery)}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    const recipesPerIngredient = 1;
+    const allRecipes = [];
 
-    try {
-        const response = await axios.get(url);
-        const recipes = response.data.hits
-            .map((hit) => ({
+    for (let i = 0; i < Math.min(5, ingredients.length); i++) {
+        const ingredient = ingredients[i];
+        const url = `https://api.edamam.com/search?q=${encodeURIComponent(ingredient)}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+        try {
+            const response = await axios.get(url);
+            const recipes = response.data.hits.slice(0, recipesPerIngredient).map((hit) => ({
                 label: hit.recipe.label,
                 url: hit.recipe.url,
                 ingredients: hit.recipe.ingredientLines,
-            }))
-            
-
-        return recipes; // Return the array of recipes that match the ingredients
-    } catch (error) {
-        console.error('Error fetching recipes:', error);
-        return []; // Return an empty array in case of error
+            }));
+            allRecipes.push(...recipes); // Add the recipes for the current ingredient to the allRecipes array
+        } catch (error) {
+            console.error(`Error fetching recipes for ${ingredient}:`, error);
+        }
     }
+
+    return allRecipes; // Return the array of all recipes
 }
 
 // Example usage
