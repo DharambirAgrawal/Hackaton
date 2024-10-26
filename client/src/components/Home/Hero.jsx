0668620a4170892b2ef28a28349275ecb1ba65ef
ghislainnkundayezu <ghislainnkundayezu@gmail.com>
2024-10-26 18:51:43 -0500

    minor

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <section className="">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 md:grid-cols-2">
         <div className="mr-auto place-self-center w-full flex flex-col items-center">
-          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-center">
+          <h1 className="w-full mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-center">
             Welcome to Nourish Now
           </h1>
           <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 text-center">
